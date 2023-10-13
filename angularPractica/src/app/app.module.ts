12606970ca9360ebd0367f723cc957ba0744a7cb
Carlo sCortes Lopez <124748197+VIIZ7@users.noreply.github.com>
2023-10-13 09:56:25 -0500

    v3

--- angularPractica/src/app/app.module.ts
+++ angularPractica/src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { LoginComponent } from './login/login.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+const appRoutes: Routes=[
+  {path:'inicio',component:InicioComponent},
+  {path:'nosotros',component:NosotrosComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing:true}
+    ),
     BrowserModule,
-    AppRoutingModule
+    MatInputModule,
+    MatFormFieldModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule, 
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
